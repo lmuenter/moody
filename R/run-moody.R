0793e1d0f3dc37fe4c3dac3727a53bfccfd921d4
lmuenter <50193931+lmuenter@users.noreply.github.com>
2021-07-24 18:28:59 +0200

    created basic function for running the app.

---
+++ R/run-moody.R
@@ -0,0 +1,25 @@
+# title: Run Moody
+# purpose: runs moody
+# date: 24.07.2021
+# author: Lukas Muenter
+
+#-------------------------------
+#' Run Moody
+#'
+#' Runs moody locally.
+#' @import shiny
+#' @export
+moody <- function() {
+
+  ## localise the app
+  appDir <- system.file("moody", "app", package = "moody")
+
+
+  ## warn if something is wrong
+  if (appDir == "") {
+    stop("Could not find the app. Try re-installing `moody`.", call. = FALSE)
+  }
+
+  ## run app
+  runApp(appDir, display.mode = "normal")
+}
